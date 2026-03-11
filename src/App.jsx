e1d0f7ea93b4cf31587a9c11507b83217d9e0f3c
Avinash <buildwithavinash@users.noreply.github.com>
2026-03-11 14:32:07 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,135 @@
+import { useState } from "react";
+
+const App = () => {
+  const [num, setNum] = useState("");
+  const [message, setMessage] = useState("Start guessing!");
+  const [userTurn, setUserTurn] = useState(3);
+  const [randomNumber, setRandomNumber] = useState(
+    () => Math.floor(Math.random() * 20) + 1,
+  );
+  const [currentLevel, setCurrentLevel] = useState("easy");
+  let levelText = "1-20";
+
+  if(currentLevel === "medium") {
+    levelText = "1-50";
+  } 
+  if (currentLevel === "hard") {
+    levelText = "1-100";
+  }
+
+  function generateRandomLevel(level){
+      if(level === "easy") return Math.floor(Math.random()*20)+1;
+  if(level === "medium") return Math.floor(Math.random()*50)+1;
+  if(level === "hard") return Math.floor(Math.random()*100)+1;
+  }
+
+
+  function handleChange(e) {
+    setNum(Number(e.target.value));
+  }
+
+  function handleClick() {
+    if (userTurn === 0) return;
+
+    const diff = Math.abs(num - randomNumber);
+    let hint = "";
+
+    if (diff === 0) {
+      setMessage("Perfect! You guessed it!");
+      setNum("");
+      return;
+    }
+
+    if (diff <= 5) {
+      hint = "Very Close!";
+    } else if (diff <= 10) {
+      hint = "Getting Closer!!";
+    }
+
+    if (num === randomNumber) {
+      setMessage("You won!!, Want to play again? Click restart");
+      return;
+    }
+
+    // conditions
+    if (num > randomNumber) {
+      hint += " (Too High)";
+    } else if (num < randomNumber) {
+      hint += " (Too Low)";
+    }
+
+    if (userTurn === 1) {
+      setMessage(`Game over! The number was ${randomNumber}`);
+    } else {
+      setMessage(hint);
+    }
+    setUserTurn((prev) => prev - 1);
+    setNum("");
+  }
+
+  function restartGame() {
+    setRandomNumber(generateRandomLevel(currentLevel));
+    setUserTurn(3);
+    setNum("");
+    setMessage("Start Guessing!");
+  }
+
+  function changeLevel(level){
+  setCurrentLevel(level);
+  setRandomNumber(generateRandomLevel(level));
+  setUserTurn(3);
+  setMessage("Start Guessing!");
+}
+
+  console.log(randomNumber);
+  return (
+    <div className="flex items-center justify-center h-screen p-4">
+      <div className="h-[85vh] bg-red-100-900 text-slate-900 flex flex-col items-center justify-center gap-8 border border-slate-400 rounded-xl p-4 mx-auto">
+      <div className="text-center">
+        <h1 className="text-red-600 font-semibold text-3xl lg:text-6xl mb-4">Guess-A-Number</h1>
+
+        <div className="flex items-center justify-center gap-4 *:border border-slate-200 *:p-2 *:cursor-pointer *:rounded-2xl mb-2">
+          <button onClick={()=> changeLevel("easy")} className="bg-green-700 text-slate-200 font-semibold">Easy (1-20)</button>
+          <button onClick={()=> changeLevel("medium")} className="bg-blue-700 text-slate-200 font-semibold">
+            Medium (1-50)
+          </button>
+          <button onClick={()=> changeLevel("hard")} className="bg-red-700 text-slate-200 font-semibold">Hard (1-100)</button>
+        </div>
+        <p className="text-slate-700">Guess a number between {levelText}</p>
+      </div>
+
+      <div className="flex gap-3">
+        <input
+          disabled={userTurn === 0}
+          type="number"
+          value={num}
+          onChange={handleChange}
+          placeholder="Guess a number"
+          className="border border-slate-700 px-4 py-2 outline-none focus:outline-1 focus:border-blue-700 rounded-2xl transition-all duration-200"
+        />
+
+        <button
+          disabled={userTurn === 0}
+          className="border border-slate-100 px-3 py-1.5 bg-green-500 rounded-2xl cursor-pointer"
+          onClick={handleClick}
+        >
+          Enter
+        </button>
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-4">
+        <p>{message}</p>
+        {userTurn === 0 ? (
+          <p>You have used all your chances, Click restart to play again</p>
+        ) : (
+          <p>You have {userTurn} chances left</p>
+        )}
+        <button onClick={() => restartGame()}>Restart</button>
+      </div>
+    </div>
+    </div>
+    
+  );
+};
+
+export default App;
